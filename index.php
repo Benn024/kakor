6c67684fb43ae26bc36e3eeddc011ba5981d3033
carekb684 <carekb684@E29645.edu.linkoping.se>
2014-09-22 10:30:06 +0200

    1

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
 <?php
+//skapa arrayt att lagra listan i i minnet
+$kakArray = array();
+
+
+// uppdatera kaka
 if (isset($_GET["produkt"])) {
 
-    
-    
+
+
+
+    //läsa formuläret
     $produkt = $_GET["produkt"];
-    $kakArray =  unserialize($_COOKIE["lista"]);
-    
-    $kakStr = serialize($kakArray);
+
+    //kolla om kakan finns redan, om inte skapa den
+    if (isset($_COOKIE["lista"])) {
+        $kakArray = unserialize($_COOKIE["lista"]);
+    } else {
+//        setcookie("lista",0,time()+3600);
+    }
+
+
+    //lägga till formulärdata längst bak
     array_push($kakArray, $produkt);
 
-    setcookie("lista", serialize($kakStr),time()+3600);
+    // gör om array t sträng för att lagra i kaka
+    $kakStr = serialize($kakArray);
 
-    var_dump($minneslista);
+    //lagra kaka
+    setcookie("lista", $kakStr, time() + 3600);
+
+   
+
+//       var_dump($_COOKIE);
+}
+
+//rensa kaka
+if (isset($_GET["rensa"])) {
+    setcookie("lista", 0, time() - 3600);
 }
 ?>
 
@@ -26,8 +51,26 @@ if (isset($_GET["produkt"])) {
     <body>
         <form>
             <input type="text" name="produkt">
-            <input type="submit" name="submit">
-
+            <input type="submit">
         </form>
+        <a href="index.php?rensa=ja">rensa</a> | <a href="index.php">nollställ</a>
+
+        <table>
+            <tr>
+
+                
+            <?php 
+             foreach ($kakArray as $temp) {
+                 echo "<td>" . $temp . "</td>";
+             }
+            
+            ?>
+             
+
+            </tr>
+
+        </table>
+
+
     </body>
 </html>
